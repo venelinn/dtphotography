3060950fdfc5899afd3a8c1d795566c7fced0178
Venelin Nikolov <venelinn@gmail.com>
2020-06-15 10:13:45 -0400

    Added feed module

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,13 +5,13 @@ module.exports = {
   siteMetadata: {
     title: `Dimitar Tsvetkov • Photographer`,
     description: `Dimitar Tsvetkov • Photographer`,
-    siteUrl: 'https://dtphotography.ca/', // Site domain. Do not include a trailing slash! If you wish to use a path prefix you can read more about that here: https://www.gatsbyjs.org/docs/path-prefix/
+    siteUrl: 'https://dtphotography.ca', // Site domain. Do not include a trailing slash! If you wish to use a path prefix you can read more about that here: https://www.gatsbyjs.org/docs/path-prefix/
     author: 'Dimitar Tsvetkov', // Author for RSS author segment and SEO schema
     authorJob: `Photographer`,
     copyright: 'Dimitar Tsvetkov Photography',
     userTwitter: '@dtphotography', // Change for Twitter Cards
     shortTitle: 'DTP', // Used for App manifest e.g. Mobile Home Screen
-    shareImage: `share.jpg`, // Open Graph Default Share Image. 1200x1200 is recommended
+    shareImage: '/images/share.jpg', // Open Graph Default Share Image. 1200x1200 is recommended
     shareImageWidth: 900, // Change to the width of your default share image
     shareImageHeight: 600, // Change to the height of your default share image
     siteLogo: `/images/favicons/android-chrome-512x512.png`, // Logo used for SEO, RSS, and App manifest
@@ -91,6 +91,80 @@ module.exports = {
         trackingId: process.env.GOOGLE_ANALYTICS,
         head: true
       }
+    },
+    {
+      resolve: 'gatsby-plugin-feed-generator',
+      options: {
+        generator: `GatsbyJS`,
+        rss: false, // Set to true to enable rss generation
+        json: true, // Set to true to enable json feed generation
+        siteQuery: `
+      {
+        site {
+          siteMetadata {
+            title
+            description
+            siteUrl
+            author
+          }
+        }
+      }
+    `,
+        feeds: [
+          {
+            name: 'feedPost',
+            query: `
+            {
+              allContentfulPost(sort: { fields: [publishDate], order: DESC }, limit: 1) {
+                edges {
+                  node {
+                  title
+                  id
+                  slug
+                  publishDate(formatString: "MMMM DD, YYYY")
+                  }
+                }
+              }
+            }
+            `,
+            normalize: ({ query: { site, allContentfulPost } }) => {
+              return allContentfulPost.edges.map(edge => {
+                return {
+                  title: edge.node.title,
+                  url: site.siteMetadata.siteUrl + site.siteMetadata.blogPath + edge.node.slug,
+                  date: edge.node.publishDate,
+                }
+              })
+            },
+          },
+          {
+            name: 'feedPhoto',
+            query: `
+            {
+              allContentfulPortfolio(sort: { fields: [createdAt], order: DESC }, limit: 1) {
+                edges {
+                  node {
+                  title
+                  id
+                  slug
+                  createdAt(formatString: "MMMM DD, YYYY")
+                  }
+                }
+              }
+            }
+            `,
+            normalize: ({ query: { site, allContentfulPortfolio } }) => {
+              return allContentfulPortfolio.edges.map(edge => {
+                return {
+                  title: edge.node.title,
+                  url: site.siteMetadata.siteUrl + '/' + edge.node.slug,
+                  date: edge.node.createdAt,
+                }
+              })
+            },
+          },
+        ],
+      },
     },
     {
       resolve: `gatsby-plugin-manifest`,
