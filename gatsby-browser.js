4261e48878d51871ed09eda772814844a8cb90fe
Venelin Nikolov <venelinn@gmail.com>
2023-11-30 22:41:54 -0500

    trying to build g5 4

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,27 +3,27 @@
 // export const wrapPageElement = CustomLayout
 // export const shouldUpdateScroll = scrollDelay
 
-import React from "react";
-import Layout from "./src/components/Layout";
+// import React from "react";
+// import Layout from "./src/components/Layout";
 
-const transitionDelay = 500;
+// const transitionDelay = 500;
 
-export const wrapPageElement = ({ element, props }) => {
-  return <Layout {...props}>{element}</Layout>;
-};
+// export const wrapPageElement = ({ element, props }) => {
+//   return <Layout {...props}>{element}</Layout>;
+// };
 
-export const shouldUpdateScroll = ({
-  routerProps: { location },
-  getSavedScrollPosition
-}) => {
-  if (location.action === "PUSH") {
-    window.setTimeout(() => window.scrollTo(0, 0), transitionDelay);
-  } else {
-    const savedPosition = getSavedScrollPosition(location);
-    window.setTimeout(
-      () => window.scrollTo(...(savedPosition || [0, 0])),
-      transitionDelay
-    );
-  }
-  return false;
-};
+// export const shouldUpdateScroll = ({
+//   routerProps: { location },
+//   getSavedScrollPosition
+// }) => {
+//   if (location.action === "PUSH") {
+//     window.setTimeout(() => window.scrollTo(0, 0), transitionDelay);
+//   } else {
+//     const savedPosition = getSavedScrollPosition(location);
+//     window.setTimeout(
+//       () => window.scrollTo(...(savedPosition || [0, 0])),
+//       transitionDelay
+//     );
+//   }
+//   return false;
+// };
